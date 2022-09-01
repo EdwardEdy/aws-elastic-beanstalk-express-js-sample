2cac42d3afc7a7feb326d9aafdf830978102bf86
EdwardEdy <112610730+EdwardEdy@users.noreply.github.com>
2022-09-01 11:14:19 +0300

    git add app.js git commit -m "Full pipeline test"
    git push

--- app.js
+++ app.js
@@ -6,6 +6,3 @@ app.get('/', (req, res) => res.send('What iz going on, iz it woking TEST TEST'))
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
-
-git add app.js
-git commit -m "Full pipeline test"
