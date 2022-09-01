067b2cb70fe25796424177780e9ca8c7715c292b
EdwardEdy <112610730+EdwardEdy@users.noreply.github.com>
2022-09-01 11:48:24 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('What iz going on, iz it woking TEST TEST'));
+app.get('/', (req, res) => res.send('This is a Test(Edward)'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
