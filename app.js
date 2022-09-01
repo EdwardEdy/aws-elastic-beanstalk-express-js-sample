6ee9d05b1ebbaba7b7dfc9020feaf2b3c40205e2
EdwardEdy <112610730+EdwardEdy@users.noreply.github.com>
2022-09-01 11:07:58 +0300

    git push

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Yellow Brothers :D :D:D!'));
+app.get('/', (req, res) => res.send('What iz going on, iz it woking TEST TEST'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
+
+git add app.js
+git commit -m "Full pipeline test"
